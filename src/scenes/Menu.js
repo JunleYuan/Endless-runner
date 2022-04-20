d568fe10c4a30cbb7b33f2b312676e99cfdc7078
Junle Yuan <76648388+JunleYuan@users.noreply.github.com>
2022-04-20 15:02:12 -0700

    Add files via upload

--- src/scenes/Menu.js
+++ src/scenes/Menu.js
@@ -28,13 +28,14 @@ class Menu extends Phaser.Scene {
     keyJump = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.W);
     keySlide = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.S);
 
-    var stars = this.physics.add.group({ allowGravity: false , immovable: true});
-    stars.add(new FlyingStar(this, 200, 300, 0.0005), true);
+    this.stars = this.physics.add.group({ allowGravity: false , immovable: true});
+    this.path = new Path(this, 200, 300, 'spaceship', 0.0005);
+    this.stars.add(this.path, true);
     
 
     //add collision with objects
     this.physics.add.collider(this.player, platforms);
-    this.physics.add.collider(this.player, stars);
+    this.physics.add.collider(this.player, this.stars);
 
   }
   
@@ -45,7 +46,7 @@ class Menu extends Phaser.Scene {
 
     //update prefeb
     this.player.update();
-
+    this.path.update();
     
     
 
