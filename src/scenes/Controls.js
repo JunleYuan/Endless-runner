b73cdaf46a5447bcaea6b9a4ae5299e45b627a37
JunleYuan <leo905991717@gmail.com>
2022-04-27 23:33:22 -0700

    button change

--- src/scenes/Controls.js
+++ src/scenes/Controls.js
@@ -4,38 +4,57 @@ class Controls extends Phaser.Scene {
   }
 
   preload() {
-    
+    this.load.bitmapFont('bm', 'assets/bm_0.png', 'assets/bm.xml');
   }
-    
+
   create() {
     // menu text configuration
     let menuConfig = {
-     fontFamily: 'Courier',
-     fontSize: '24px',
-     backgroundColor: '#000000',
-     color: '#FFFFFF',
-     align: 'center',
-     padding: {
+      fontFamily: 'Courier',
+      fontSize: '24px',
+      backgroundColor: '#000000',
+      color: '#FFFFFF',
+      align: 'center',
+      padding: {
         top: 5,
         bottom: 5,
-     },
-     fixedWidth: 0
+      },
+      fixedWidth: 0
     }
-    
+
     //controls text
-    this.controlsText = this.add.text(game.config.width/2, game.config.height/2, '', menuConfig).setOrigin(0.5,0.5);
-    this.controlsText.text = 'Press A to move left, and press D to move right \n\n Press W to jump, and W again while in the air to double jump \n\n Press S to slide under obstacles';
+    let controlsText = this.add.bitmapText(game.config.width / 2, game.config.height / 2, 'bm', 'Press A to move left, and press D to move right \n\nPress W to jump, and W again while in the air to double jump \n\nPress S to slide under obstacles', 15).setOrigin(0.5, 0.5);
+    //this.controlsText = this.add.text(game.config.width/2, game.config.height/2, '', menuConfig).setOrigin(0.5,0.5);
+    //this.controlsText.text = 'Press A to move left, and press D to move right \n\n Press W to jump, and W again while in the air to double jump \n\n Press S to slide under obstacles';
 
     //menu button
-    menuConfig.backgroundColor = '#DD002F';
-    this.menuButton = this.add.text(game.config.width/2, game.config.height/1.5, 'Main Menu', menuConfig).setOrigin(0.5,0).setInteractive();
-    this.menuButton.on('pointerdown', () => {
-        this.scene.start('menuScene'); 
+    let menuButton = this.add.bitmapText(game.config.width / 2, game.config.height / 1.5, 'bm', 'Main Menu', 34).setOrigin(0.5);
+    menuButton.setInteractive();
+    // this.menuButton = this.add.text(game.config.width/2, game.config.height/1.5, 'Main Menu', menuConfig).setOrigin(0.5,0).setInteractive();
+    // this.menuButton.on('pointerdown', () => {
+    //     this.scene.start('menuScene'); 
+    // });
+
+    menuButton.on('pointerdown', () => {
+      this.scene.start("playingScene");
+      isGameOver = false;
+      hit_count = 0;
     });
+
+    menuButton.on('pointerover', () => {
+      menuButton.setScale(1.2);
+
+    });
+    menuButton.on('pointerout', () => {
+      menuButton.setScale(1);
+
+    });
+
+
   }
-    
+
   update(time, delta) {
 
   }
-    
+
 }
