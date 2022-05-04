9fb3fbee0f1c4c6d11f3701059975f2ece6a96d9
JunleYuan <leo905991717@gmail.com>
2022-05-04 16:39:50 -0700

    fixed end game

--- src/scenes/End.js
+++ src/scenes/End.js
@@ -32,12 +32,21 @@ class End extends Phaser.Scene {
         let scoreDisplay = this.add.bitmapText(game.config.width / 2, game.config.height/2, 'bm','Score: ' + runScore.toFixed(0), 34).setOrigin(0.5);
 
         this.clock = this.time.delayedCall(3000, () => {
-        
+            mnMusic = this.sound.add('MenuMusic',{volume: 0.3});
+            mnMusic.loop = true; // Sets Loop
+            mnMusic.play();
+
+
             this.ed = this.add.sprite(0, 0).play('end').setOrigin(0,0).setScale(.5399);
             this.goMenu = this.add.text(860, 450, ' ').setOrigin(0.5,0).setInteractive();
             //this.goMenu.setBackgroundColor('#DD002F');
             this.goMenu.setDisplaySize(70, 40);
             this.goMenu.on('pointerdown', () => {
+                isGameOver = false;
+                hit_count = 0;
+                nubToast = 0;
+                playerGotHit = false;
+                mnMusic.stop();
                 this.scene.start('menuScene'); 
             });
         }, null, this);
